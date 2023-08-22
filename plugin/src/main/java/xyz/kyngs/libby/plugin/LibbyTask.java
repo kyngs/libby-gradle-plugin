0edcdf75a06f41ab40c00ea1ea41201ec63b4b66
kyngs <kyngs@users.noreply.github.com>
2023-08-22 17:11:59 +0200

    Initial commit

---
+++ plugin/src/main/java/xyz/kyngs/libby/plugin/LibbyTask.java
@@ -0,0 +1,11 @@
+package xyz.kyngs.libby.plugin;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.TaskAction;
+
+public class LibbyTask extends DefaultTask {
+    @TaskAction
+    public void run() {
+
+    }
+}
